5ef933ec6c96f27676737b1b4562277a70b1b33d
RepComm <rccommandotac@gmail.com>
2020-07-19 22:23:13 -0500

    Removed docs in favor of thrice-js output, now using /build/ folder, gitignored /build/

--- docs/rendering/scene.js
+++
@@ -1,7 +0,0 @@
-import { Object3D } from "./object3d.js";
-export class Scene extends Object3D {
-  constructor() {
-    super();
-  }
-
-}
--- docs/rendering/shaders/demo-frag.glsl
+++
@@ -1,8 +0,0 @@
-
-/** This program decides what color each pixel that gets rendered is
-  * Commonly executed for every pixel in a triangle
-  */
-
-void main(){
-  gl_FragColor=vec4(1.0, 1.0, 1.0, 1.0);
-}
